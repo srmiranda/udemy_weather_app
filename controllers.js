a1b1b3275429179ae741e2e8eea2e5043a342fcc
SasquatchShawn <srmiranda1@gmail.com>
2015-12-21 21:00:43 -0500

    Added the get request from the weather api to the separate service.js file

--- controllers.js
+++ controllers.js
@@ -14,15 +14,12 @@ weatherApp.controller('homeController', ['$scope', '$location', '$log', 'citySer
     
 }]);
 
-weatherApp.controller('forcastController', ['$scope', '$log', '$resource', '$routeParams', 'cityService', function($scope, $log, $resource, $routeParams, cityService) {
+weatherApp.controller('forcastController', ['$scope', '$log', '$routeParams', 'cityService', 'weatherService', function($scope, $log, $routeParams, cityService, weatherService) {
     
     $scope.city = cityService.city;
     $scope.days = $routeParams.days || '2';
     
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
-        callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
-    
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, APPID: '7142da4098dac47b689d011c59fb174e' });
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
     
     $scope.convertToF = function(degK) {
         return Math.round((1.8 * (degK - 273)) + 32); 
@@ -30,7 +27,7 @@ weatherApp.controller('forcastController', ['$scope', '$log', '$resource', '$rou
     
     $scope.convertDate = function(dt) {
         return new Date(dt * 1000);
-    }
+    };
     
 
 }]);
--- services.js
+++ services.js
@@ -5,3 +5,17 @@ weatherApp.service('cityService', function() {
     this.city = "Marstons Mills, MA";
     
 })
+
+// Removed from Forcast Controller and Added here
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+    
+    this.GetWeather = function(city, days) {
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+        callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    return weatherAPI.get({ q: city, cnt: days, APPID: '7142da4098dac47b689d011c59fb174e' });
+        
+    };
+    
+}]);
